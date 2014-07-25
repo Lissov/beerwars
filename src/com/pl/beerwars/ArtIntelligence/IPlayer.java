5c736f6899599712f9b5d1ed6273295f2960370f
lissov <pavlo.lissov@gmail.com>
2014-07-25 18:36:08 +0200

    Interface for player

---
+++ src/com/pl/beerwars/ArtIntelligence/IPlayer.java
@@ -0,0 +1,8 @@
+package com.pl.beerwars.ArtIntelligence;
+import com.pl.beerwars.data.facade.*;
+import com.pl.beerwars.data.playerdata.*;
+
+public interface IPlayer
+{
+	void MakeTurn(PlayerData data);
+}
---
+++ src/com/pl/beerwars/ArtIntelligence/Normal/NormalPlayer.java
@@ -0,0 +1,10 @@
+package com.pl.beerwars.ArtIntelligence.Normal;
+import com.pl.beerwars.ArtIntelligence.*;
+import com.pl.beerwars.data.playerdata.*;
+
+public class NormalPlayer implements IPlayer
+{
+	public void MakeTurn(PlayerData data){
+	
+	}
+}
